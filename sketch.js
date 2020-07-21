50f880bff4641c96508ce948f8a84841d95b9640
JaessScript <jaess.script@gmail.com>
2020-07-21 15:18:54 +0200

    Add css

--- sketch.js
+++ sketch.js
@@ -2,6 +2,11 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 }
 
+// Resive the canvas when the size of the browser changes
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+
 function draw() {
 
 }
