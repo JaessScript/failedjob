cb208e5a4d9a536f85ef5bc59a6eb86d81da06a2
JaessScript <jaess.script@gmail.com>
2020-07-21 14:10:17 +0200

    Adding html and js of the results page

---
+++ results.js
@@ -0,0 +1,38 @@
+let max = 25;
+let min = 3;
+
+let confidence = document.getElementById('confidence');
+let confidence_val = Math.floor(Math.random()*(max-min+1)+min);
+confidence.setAttribute("value", confidence_val);
+
+let motivation = document.getElementById('motivation');
+let motivation_val = Math.floor(Math.random()*(max-min+1)+min);
+motivation.setAttribute("value", motivation_val);
+
+let leadership = document.getElementById('leadership');
+let leadership_val = Math.floor(Math.random()*(max-min+1)+min);
+leadership.setAttribute("value", leadership_val);
+
+let attention = document.getElementById('attention');
+let attention_val = Math.floor(Math.random()*(max-min+1)+min);
+attention.setAttribute("value", attention_val);
+
+let assertiveness = document.getElementById('assertiveness');
+let assertiveness_val = Math.floor(Math.random()*(max-min+1)+min);
+assertiveness.setAttribute("value", assertiveness_val);
+
+let teamplayer = document.getElementById('teamplayer');
+let teamplayer_val = Math.floor(Math.random()*(max-min+1)+min);
+teamplayer.setAttribute("value", teamplayer_val);
+
+let honesty = document.getElementById('honesty');
+let honesty_val = Math.floor(Math.random()*(max-min+1)+min);
+honesty.setAttribute("value", honesty_val);
+
+let pbsolving = document.getElementById('pbsolving');
+let pbsolving_val = Math.floor(Math.random()*(max-min+1)+min);
+pbsolving.setAttribute("value", pbsolving_val);
+
+let ambition = document.getElementById('ambition');
+let ambition_val = Math.floor(Math.random()*(max-min+1)+min);
+ambition.setAttribute("value", ambition_val);
